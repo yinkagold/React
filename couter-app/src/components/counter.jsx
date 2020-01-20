cae9328fe34dcd8c9cb595658ddacc5db9c949a2
Yinka Gold <yinkagolden@gmail.com>
2020-01-21 01:50:27 +0200

    Counter App

---
+++ couter-app/src/components/counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1
+  };
+
+  render() {
+    let classes = this.getBadgeClasses();
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
