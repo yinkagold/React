bc7e42d3ceaf53612e27e871e9eb53bf608c18e9
Yinka Gold <yinkagolden@gmail.com>
2020-01-21 16:52:22 +0200

    Replacing constructor with arrow function

--- couter-app/src/components/counter.jsx
+++ couter-app/src/components/counter.jsx
@@ -8,8 +8,9 @@ class Counter extends Component {
   // super();
   //this.handleIncrement = this.handleIncrement.bind(this);
   //}
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
@@ -17,7 +18,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
