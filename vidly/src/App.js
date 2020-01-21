310d164260f739ffbaad929fae271a9eb38aa66b
Yinka Gold <yinkagolden@gmail.com>
2020-01-21 22:31:04 +0200

    Vidly app to display movies

---
+++ vidly/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Movie from "./components/movies";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
