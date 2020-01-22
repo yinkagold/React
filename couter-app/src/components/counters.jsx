dabd6350e676add8e115f0507bac5e7d477f2a6c
Yinka Gold <yinkagolden@gmail.com>
2020-01-22 12:47:18 +0200

    Adding and implementing a reset button

--- couter-app/src/components/counters.jsx
+++ couter-app/src/components/counters.jsx
@@ -10,7 +10,21 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
-
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0; // reset value to 0
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; // here the array is cloned
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    // console.log(this.state.counters[index]);
+  };
   handleDelete = countrerId => {
     const counters = this.state.counters.filter(c => c.id !== countrerId);
     this.setState({ counters });
@@ -18,10 +32,14 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm-2">
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter} // this carries all the data about the data
           />
         ))}
