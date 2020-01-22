c20833e2a037307fe3c481a0d59019a890adced9
Yinka Gold <yinkagolden@gmail.com>
2020-01-22 12:45:54 +0200

    Removing the state and using props

--- couter-app/src/components/counter.jsx
+++ couter-app/src/components/counter.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
+  // Here we remove the state property and use the props to display data and notify changes
   render() {
     console.log("props", this.props);
     return (
@@ -16,14 +9,14 @@ class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm-m-2"
+          className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
@@ -33,12 +26,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
