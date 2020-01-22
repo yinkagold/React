fb3ec8e902be21ffa4673ae8460a27e01b1b5694
Yinka Gold <yinkagolden@gmail.com>
2020-01-22 13:28:58 +0200

    Adding multiple component in sync and using props

--- couter-app/src/App.js
+++ couter-app/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Counters from "./components/counters";
+import Counter from "./components/counter";
+import NavBar from "./components/navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 }, // id uniquely identifies the counter, values to set the initial value
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0; // reset value to 0
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; // here the array is cloned
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    // console.log(this.state.counters[index]);
+  };
+  handleDelete = countrerId => {
+    const counters = this.state.counters.filter(c => c.id !== countrerId);
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- couter-app/src/components/counters.jsx
+++ couter-app/src/components/counters.jsx
@@ -2,44 +2,20 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 }, // id uniquely identifies the counter, values to set the initial value
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0; // reset value to 0
-      return c;
-    });
-    this.setState({ counters });
-  };
-  handleIncrement = counter => {
-    const counters = [...this.state.counters]; // here the array is cloned
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-    // console.log(this.state.counters[index]);
-  };
-  handleDelete = countrerId => {
-    const counters = this.state.counters.filter(c => c.id !== countrerId);
-    this.setState({ counters });
-  };
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn btn-primary btn-sm-2">
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter} // this carries all the data about the data
           />
         ))}
