c735f68bb825c5d08668afee06f1a12cd0013c8a
Yinka Gold <yinkagolden@gmail.com>
2020-01-28 12:44:24 +0200

    Working pagination with Listgroup

--- vidly/src/utils/paginate.js
+++ vidly/src/utils/paginate.js
@@ -3,6 +3,7 @@ import _ from "lodash";
 export function paginate(items, pageNumber, pageSize) {
   const startIndex = (pageNumber - 1) * pageSize;
   return _(items)
-    .slice(startIndex)
-    .take(pageSize).value; // paginating data on the client side
+    .slice(startIndex) // slice the array from that startIndex
+    .take(pageSize)
+    .value(); // paginating data on the client side
 }
