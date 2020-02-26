44d84b6b05540d172c8d3b1335c1c1d1b0ace80e
Yinka Gold <yinkagolden@gmail.com>
2020-02-26 14:49:51 +0200

    Calling backend services using axios

---
+++ http-app/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+const apiEndpoint = "http://jsonplaceholder.typicode.com/posts";
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    const { data: posts } = await axios.get(apiEndpoint);
+    this.setState({ posts });
+  }
+  handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+
+    const posts = [post, ...this.state];
+    this.setState({ posts });
+  };
+
+  handleUpdate = async post => {
+    post.title = "UPDATED";
+    await axios.put(apiEndpoint + "/" + post.id, post);
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(posts);
+    posts[index] = { ...post };
+    console.log(posts);
+    // axios.patch(apiEndpoint + '/' + post.id, { title: post.title });
+  };
+
+  handleDelete = async post => {
+    await axios.delete(apiEndpoint + "/" + post.id);
+    const posts = this.state.posts.filter(p => p.id);
+    this.setState({ posts });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
