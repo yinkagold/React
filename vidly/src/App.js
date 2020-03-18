8182e7acb20ec2552c0ddc937f622646a6f2853b
Yinka Gold <yinkagolden@gmail.com>
2020-03-18 13:09:26 +0200

    Rafactoring and updating logout component

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import Movies from "./components/movies";
 import Customers from "./components/customer";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import LoginForm from "./components/loginForm";
-import "./App.css";
+import Logout from "./components/logout";
+import RegisterForm from "./components/registerForm";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
+import auth from "./services/authService";
+import "react-toastify/dist/ReactToastify";
+import "./App.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path="/login" component={LoginForm}></Route>
-          <Route path="/movies/:id" component={MovieForm}></Route>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar user={this.state.user} />
+        <ToastContainer />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout}></Route>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
